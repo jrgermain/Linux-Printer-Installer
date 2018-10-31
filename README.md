3e5eacbe2540cee4fcc0385c58304a893ee552e9
jrgermain <jrgermain@github.com>
2018-10-31 13:30:37 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# qu-printing
+
+This shell script installs Quinnipiac's new printers on a Linux or macOS machine.
+
+
+### Why?
+
+While we have packaged macOS and Windows installers, we don't have installers available for Linux or older versions of Mac OS X.
+
+### How?
+
+I made a shell script that uses the `lpadmin` command to install the printers. The official Mac installer uses the same commands.
+
+### Is it free?
+
+Yes. Tuition is expensive enough as it is. Plus it's open-source!
+
+### How do I use it?
+
+Run the shell script normally. In a terminal, type ` /dir/qu-printing.sh ` where ` dir ` is the directory the script is in.
+
+For example, if the file is in your Downloads folder, run
+
+`~/Downloads/qu-printing.sh`
+
+And follow all on-screen prompts.
---
+++ qu-printing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Old-school printer install shell script for Quinnipiac campus printing
+# Works with Linux and macOS
+#
+# Written by Joey Germain
+# 10/31/2018
+#
+# TODO: verify that printers actually install, remove old printers, add option to use command-line parameters as username
+
+
+#prompt for username
+echo "Please enter your Quinnipiac network username: "
+read uname
+
+#strip @qu.edu or @quinnipiac.edu from username by ignoring anything after '@'
+uname=${uname%@*}
+
+#download printer drivers
+curl -s http://is.quinnipiac.edu/installs/MacCampusPrinting.zip --output /tmp/MacCampusPrinting.zip
+curl -s http://is.quinnipiac.edu/installs/MacCampusPrintingColor.zip --output /tmp/MacCampusPrintingColor.zip
+
+#unzip printer drivers
+unzip -qq -o /tmp/MacCampusPrinting.zip -d /tmp
+unzip -qq -o /tmp/MacCampusPrintingColor.zip -d /tmp
+
+#install printers
+lpadmin -p "CampusPrinting" -v "lpd://${uname}@mfdServer/maccampusprinting" -P "/tmp/MacCampusPrinting.ppd" -o printer-is-shared=false
+cupsenable "CampusPrinting" -E; cupsaccept "CampusPrinting"
+
+lpadmin -p "CampusPrintingColor" -v "lpd://${uname}@mfdServer/maccampusprintingcolor" -P "/tmp/MacCampusPrintingColor.ppd" -o printer-is-shared=false
+cupsenable "CampusPrintingColor" -E; cupsaccept "CampusPrintingColor"
+
+#delete temporary files
+rm /tmp/MacCampusPrinting*
+
+#print success message regardless of whether printers actually install (for now)
+echo "Printers installed sucessfully."
